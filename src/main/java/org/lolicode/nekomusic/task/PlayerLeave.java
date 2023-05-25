9d90d362a93763117707cd2786a0decd020d5493
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2023-05-25 12:01:36 +0800

    add delay after first player join
    so we can receive hello packet

--- src/main/java/org/lolicode/nekomusic/task/PlayerLeave.java
+++ src/main/java/org/lolicode/nekomusic/task/PlayerLeave.java
@@ -3,9 +3,16 @@ package org.lolicode.nekomusic.task;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.lolicode.nekomusic.NekoMusic;
+import org.lolicode.nekomusic.manager.PlayerManager;
 
 public class PlayerLeave {
     public static void OnPlayerLeave(ServerPlayerEntity player, MinecraftServer server) {
         NekoMusic.nekoPlayerSet.remove(player);
+        if (PlayerManager.getOnlineRealPlayerList(server).size() == 0) {
+            if (NekoMusic.task != null) {
+                NekoMusic.task.cancel();
+                NekoMusic.task = null;
+            }
+        }
     }
 }

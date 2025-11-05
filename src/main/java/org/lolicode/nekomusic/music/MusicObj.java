7c18c42fd4ab4940bf126674aede05073e8d557f
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2025-11-06 04:37:03 +0800

    fix: json parse error
    A love letter to JavaScript enthusiasts:
    "null" (string) vs null (type)

--- src/main/java/org/lolicode/nekomusic/music/MusicObj.java
+++ src/main/java/org/lolicode/nekomusic/music/MusicObj.java
@@ -1,7 +1,10 @@
 package org.lolicode.nekomusic.music;
 
+import com.google.gson.*;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class MusicObj {
@@ -22,12 +25,36 @@ public class MusicObj {
         public String picUrl;
     }
 
+    private static final class FreeTrialInfoAdapter implements JsonDeserializer<FreeTrialInfoObj> {
+        @Override
+        public MusicObj.FreeTrialInfoObj deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext ctx)
+                throws JsonParseException {
+            if (json == null || json.isJsonNull()) {
+                return null;
+            }
+//            if (json.isJsonPrimitive()) {
+//                JsonPrimitive prim = json.getAsJsonPrimitive();
+//                if (prim.isString() && "null".equalsIgnoreCase(prim.getAsString().trim())) {
+//                    return null;
+//                }
+//                // Any other primitive is illegal here; ignore
+//                return null;
+//            }
+            if (json.isJsonObject()) {
+                return ctx.deserialize(json.getAsJsonObject(), MusicObj.FreeTrialInfoObj.class);
+            }
+            // Unknown shape; ignore
+            return null;
+        }
+    }
+
     public List<ArtistObj> ar = List.of();
     public String name;
     public long id;
     public String url;
     public long dt;
     protected long time; // should be equal to dt, to determine if the song is trial (fuck netease)
+    @JsonAdapter(FreeTrialInfoAdapter.class)
     @SerializedName("freeTrialInfo")
     protected FreeTrialInfoObj freeTrialInfo;
     protected byte fee;
